f684f1de040c4327f524263c89ce9b3c59cda406
mahdimortuza <mahdi.webx@gmail.com>
2024-04-19 23:38:41 +0600

    ant design configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import { Button } from "antd";
+// const { Title } = Typography;
+
+import { Button, Card, Space } from "antd";
 
 export default function Home() {
   return (
     <main>
-      <Button type="primary">Click</Button>
+      <Space direction="vertical" size={16}>
+        <Card title="DEfault card">
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+
+        <Card
+          size="small"
+          title="Small size card"
+          extra={<a href="#">More</a>}
+          style={{ width: 300 }}
+        >
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </Space>
+      <Button type="primary">Hi</Button>
     </main>
   );
 }
